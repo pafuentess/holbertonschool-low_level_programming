6ae524141a09fd190995116ff318702e33ea54b2
pafuentess <pafuentess@unal.edu.co>
2019-09-18 20:37:16 +0000

    6-variable_declarationfix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ long long1;
 double double1;
 float float1;
 
-printf ("Size of a char: %lu byte(s)\n", sizeof(char1);
-printf ("Size of an int: %lu byte(s)\n", sizeof(int1);
-printf ("Size of a long int: %lu byte(s)\n", sizeof(long1);
-printf ("Size of a long long int: %lu byte(s)\n", sizeof(double1);
-printf ("Size of a float: %lu byte(s)\n", sizeof(float1);
+printf ("Size of a char: %ld byte(s)\n", sizeof(char1);
+printf ("Size of an int: %ld byte(s)\n", sizeof(int1);
+printf ("Size of a long int: %ld byte(s)\n", sizeof(long1);
+printf ("Size of a long long int: %ld byte(s)\n", sizeof(double1);
+printf ("Size of a float: %ld byte(s)\n", sizeof(float1);
 
 return (0);
 }
