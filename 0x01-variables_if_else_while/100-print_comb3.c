3a0dc00c9806f34f52b18d6e4af52139563a3c1b
pafuentess <pafuentess@unal.edu.co>
2019-09-19 22:45:20 +0000

    answer100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+int b;
+
+for (a = 0 ; a <= 9 ; ++a)
+{
+for (b = 1 ; b <= 9 ; ++b)
+for (a < b && a != b)
+{
+putchar(a + '0');
+putchar(b + '0');
+
+if (a == 8 && b == 9)
+{
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+
+}
