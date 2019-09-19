ed9409ca12dab09ff2efcd1fad056247bf0744a1
pafuentess <pafuentess@unal.edu.co>
2019-09-19 20:38:22 +0000

    answer9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+while (a <= 9)
+{
+putchar(a + '0');
+if (a != 9)
+{
+putchar(',');
+putchar(' ');
+}
+++a;
+}
+
+putchar('\n');
+
+return (0);
+
+}
