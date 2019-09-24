44b26d1b75a53993c2e9afa36d43987a8c1e0c3a
pafuentess <pafuentess@unal.edu.co>
2019-09-24 14:59:40 +0000

    answer3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* _islower -  identify is c is lowercase or uppercase
+* @c: the value to evaluate
+* Return: (a)
+*/
+
+int _islower(int c)
+{
+	int a, b;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		a = 1;
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		b = 0;
+	}
+return (a);
+}
+
