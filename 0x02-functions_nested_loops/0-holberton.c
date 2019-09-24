cf2fcda7614b8c28e21ddeee7aa62c64c3bb0c02
pafuentess <pafuentess@unal.edu.co>
2019-09-24 11:35:30 +0000

    answer 0 and Readme

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+* main - Print the word Holberton
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char a, b, c, d, e, f, g, h;
+
+	a = 'H';
+	b = 'o';
+	c = 'l';
+	d = 'b';
+	e = 'e';
+	f = 'r';
+	g = 't';
+	h = 'n';
+
+	_putchar(a);
+	_putchar(b);
+	_putchar(c);
+	_putchar(d);
+	_putchar(e);
+	_putchar(f);
+	_putchar(g);
+	_putchar(b);
+	_putchar(h);
+	_putchar('\n');
+
+return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0-holberton.c : print the word holberton
