eba09bf308ee1abf2a55ef76bef0a1ed382f1671
pafuentess <pafuentess@unal.edu.co>
2019-09-24 17:35:51 +0000

    answer9 with one space

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,6 @@ void times_table(void)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
 				}
 			}
 			else if (c < 10)
@@ -37,7 +36,6 @@ void times_table(void)
 				if (b != 9)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 				}
 
