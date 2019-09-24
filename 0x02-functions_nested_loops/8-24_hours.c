3662cb080e0279d5a63352c80d969103b0507825
pafuentess <pafuentess@unal.edu.co>
2019-09-24 19:27:43 +0000

    answer8 fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+ #include "holberton.h"
 
 /**
-* jack_bauer - prints every minute of the day of Jack Bauer.
+* jack_bauer - prints every minute of the day of Jack Bauer
 */
 
 void jack_bauer(void)
@@ -13,30 +13,30 @@ void jack_bauer(void)
 	while (a <= 2)
 	{
 		b = 0;
-
-		while (b <= 3)
+		while (b <= 9)
 		{
 			c = 0;
-
-			while (c <= 5)
+			if (a == 2 && b == 4)
 			{
-				d = 0;
-
-				while (d <= 9)
-				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-
-					++d;
-				}
-				++c;
+				break;
 			}
-			++b;
+				while (c <= 5)
+				{
+					d = 0;
+					while (d <= 9)
+					{
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+						++d;
+					}
+					++c;
+				}
+				++b;
 		}
-		++a;
+		++a
 	}
 }
