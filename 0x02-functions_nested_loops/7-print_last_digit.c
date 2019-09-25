6e8160592086138f2d8df1e8974827fde73ad529
pafuentess <pafuentess@unal.edu.co>
2019-09-25 15:19:37 +0000

    fix7otravezotravez3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,22 +9,22 @@
 
 int print_last_digit(int n)
 {
-	
+
 	n = n % 10;
-	
+
 	if (n < 0)
 	{
 		n = n * (-1);
 		_putchar(n + '0');
 	return (n);
-		
+
 	}
 
 	else
 	{
 		_putchar(n + '0');
 		return (n);
-		
+
 	}
 
 }
