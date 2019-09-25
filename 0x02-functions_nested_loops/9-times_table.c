a06660afb5f6b1d6826cb0fe0abce37e7b67fdc0
pafuentess <pafuentess@unal.edu.co>
2019-09-25 02:58:15 +0000

    amswer9 fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,42 +7,32 @@ void times_table(void)
 	int a, b, c, d, e;
 
 	while (a <= 9)
-	{
-		b = 0;
+	{	b = 0;
 		while (b <= 9)
-		{
-			c = a * b;
+		{	c = a * b;
 			if (c <= 9)
 			{
 				if (b != 0)
-				{
-					_putchar(' ');
+				{	_putchar(' ');
 					_putchar(' ');
 					_putchar(c + '0');
 					if (b < 9)
-					{
-						_putchar(',');
-					}
-					else
-					{
+					{	_putchar(',');
 					}
 				}
 				else
-				{
-					_putchar(c + '0');
+				{	_putchar(c + '0');
 					_putchar(',');
 				}
 			}
 			if (c > 9)
-			{
-				d = c / 10;
+			{	d = c / 10;
 				e = c % 10;
 				_putchar(' ');
 				_putchar(d + '0');
 				_putchar(e + '0');
 				if (b < 9)
-				{
-					_putchar(',');
+				{	_putchar(',');
 				}
 				else
 				{
