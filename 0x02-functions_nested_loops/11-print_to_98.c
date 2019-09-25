88ff197e5fa06ef245b53f9de6bacb9d6a11dae2
pafuentess <pafuentess@unal.edu.co>
2019-09-25 21:27:23 +0000

    answer11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - computes the number until n.
+* @n: the value to start
+*/
+
+void print_to_98(int n)
+{
+	int a;
+
+	if (n <= 98)
+	{
+		for (a = n ; a <= 98 ; ++a)
+		{
+			if (a == 98)
+			{
+				printf("%i\n", a);
+			}
+			else
+			{
+				printf("%i, ", a);
+			}
+		}
+	}
+	else
+	{
+		for (a = n ; a >= 98 ; --a)
+		{
+			if (a == 98)
+			{
+				printf("%i\n ", a);
+			}
+			else
+			{
+				printf("%i, ", a);
+			}
+		}
+	}
+	printf("\n");
+}
