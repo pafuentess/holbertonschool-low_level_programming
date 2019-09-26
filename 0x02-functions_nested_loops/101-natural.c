95ea0a51bc148f03da59ff997b1fec7bb8b02057
pafuentess <pafuentess@unal.edu.co>
2019-09-26 03:48:06 +0000

    answer101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+
+/**
+* main - prints the add of multiples of
+* 5 and 3.
+* Return (0)
+*/
+
+int main(void)
+{
+	int a, b, c, suma;
+
+	for (a = 1 ; a < 1024 ; ++a)
+	{
+		b = a % 3;
+		c = a % 5;
+
+		if (b == 0)
+		{
+			suma = suma + b;
+		}
+
+		else if (c == 0)
+		{
+			suma = suma + c;
+		}
+	}
+	printf("%i\n", suma);
+}
