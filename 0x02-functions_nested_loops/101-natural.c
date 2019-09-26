23f72b540dc1e93b2ac311077714c5267ac26c8e
pafuentess <pafuentess@unal.edu.co>
2019-09-26 04:12:03 +0000

    fix the double add that i have

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,10 +12,11 @@ int main(void)
 
 	for (a = 1 ; a < 1024 ; ++a)
 	{
-		if ((a % 3 == 0) || (a % 5 == 0))
+		if ((a % 3) == 0 || (a % 5) == 0)
 		{
 			suma = suma + a;
 		}
+
 	}
 	printf("%i\n", suma);
 
