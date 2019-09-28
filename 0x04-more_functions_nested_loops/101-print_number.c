39bf6c3e4563ad6ffee105a09552a39671b53d30
pafuentess <pafuentess@unal.edu.co>
2019-09-28 19:07:08 +0000

    answer101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,86 @@
+#include "holberton.h"
+
+/*
+* num_9 - prints number
+* @n: number to print
+*/
+
+void num_9(int n)
+{
+	_putchar(n + '0');
+}
+
+void mayor9(int n)
+{
+	int a, b;
+
+	a = n / 10;
+	b = n % 10;
+
+	_putchar(a + '0');
+	_putchar(b + '0');
+}
+
+void mayor99(int n)
+{
+	int a, b, c;
+
+	a = n / 100;
+	b = n % 100;
+	_putchar(a + '0');
+	mayor9(b);
+}
+
+void mayor999(int n)
+{
+	int a, b, c;
+
+	a = n / 1000;
+	b = n % 1000;
+	_putchar(a + '0');
+	mayor99(b);
+}
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = n * (-1);
+		_putchar('-');
+		if (n < 9)
+		{
+			num_9(n);
+		}
+		else if (n > 9 && n <= 99)
+		{
+			mayor9(n);
+		}
+		else if (n > 99 && n <= 999)
+		{
+			mayor99(n);
+		}
+		else if (n > 999 && n <= 9999)
+		{
+			mayor999(n);
+		}
+	}
+	else if (n >= 0)
+	{
+		if (n < 9)
+		{
+			num_9(n);
+		}
+		else if (n > 9 && n <= 99)
+		{
+			mayor9(n);
+		}
+		else if (n > 99 && n <= 999)
+		{
+			mayor99(n);
+		}
+		else if (n > 99 && n <= 9999)
+		{
+			mayor999(n);
+		}
+	}
+}
