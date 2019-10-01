16fcc08016d7fa72c75d9ee689f049f93602c8d6
pafuentess <pafuentess@unal.edu.co>
2019-10-01 13:51:28 +0000

    answer4 fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,49 +1,23 @@
 #include "holberton.h"
 
 /**
-* _strlen - gives the lenght of string
-* @s: the address of string to evaluate
-* Return: (a) the lenght of string
+* print_rev - print inverse a string
+* @s: string to print
 */
 
-int _strlen(char *s)
-{
-	int a;
-
-	for (a = 0 ; s[a] != '\0' ; a++)
-	{
-		 a = a + 1;
-	}
-
-	return (a);
-
-}
-
-/**
-* print_rev - print in reverse a string
-* @s: the address of string to print
-*/
 
 void print_rev(char *s)
 {
-	int a, b, c, d, e;
+	int a;
 
-	a = _strlen(s);
-
-	for (b = a ; b >= 0 ; b--)
+	while (s[a] != '\0')
 	{
-		c = s[b];
-
-		if (c == '\0')
-		{
-			d = b;
-		}
+		a = a + 1;
 	}
 
-	for (e = d ; e >= 0 ; e--)
+	for ((a = a - 1) ; a >= 0 ; a--)
 	{
-		_putchar(s[e]);
+		_putchar(s[a]);
 	}
-
 	_putchar('\n');
 }
