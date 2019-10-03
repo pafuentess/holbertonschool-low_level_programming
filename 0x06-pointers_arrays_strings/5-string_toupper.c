584f4fb5690d755414cf4c1c427f281091621e30
pafuentess <pafuentess@unal.edu.co>
2019-10-03 21:47:41 +0000

    initialize a

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,22 +1,23 @@
 #include "holberton.h"
-
 /**
 * string_toupper - change from lowercase to uppercase
 * @s: string to evaluate
 * Return: (s) string actualizado
 */
 
+
 char *string_toupper(char *s)
 {
 	int a;
 
+	a = 0;
 	while (s[a])
 	{
 		if (s[a] >= 97 && s[a] <= 122)
 		{
 			s[a] = s[a] - 32;
 		}
-		++a;
+		a++;
 	}
 	return (s);
 }
