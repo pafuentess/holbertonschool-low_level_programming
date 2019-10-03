ee347dbf9ec8d62288a5408d0f830ad8fcee18e2
pafuentess <pafuentess@unal.edu.co>
2019-10-03 20:20:30 +0000

    answer3 fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,6 @@
 #include "holberton.h"
 
 /**
-* _strlen - define the leng of a array
-* @s: array to evaluate
-* Return: (a): the lenght of array
-*/
-
-int _strlen(char *s)
-{
-	int a;
-
-	a = 0;
-
-	while (s[a] != '\0')
-	{
-		++a;
-	}
-
-	return (a);
-}
-
-/**
 * _strcmp - compares to strings
 * @s1: string to compare
 * @s2: string to compare
@@ -29,25 +9,29 @@ int _strlen(char *s)
 
 int _strcmp(char *s1, char *s2)
 {
-	int a, b, c;
+	int a, c;
 
-	a = _strlen(s1);
-	b = _strlen(s2);
+	c = 0;
+	a = 0;
 
-	if (a < b)
+	while (s1[a] != '\0' && s2[a] != '\0')
 	{
-		c = -15;
-	}
+		if (s1[a] == s2[a])
+		{
+			c = c + 0;
+		}
 
-	else if (a > b)
-	{
-		c = 15;
-	}
+		else if (s1[a] > s2[a])
+		{
+			c = c + 1;
+		}
 
-	else if (a == b)
-	{
-		c = 0;
-	}
+		else if (s1[a] < s2[a])
+		{
+			c = c - 1;
+		}
+		++a;
 
+	}
 	return (c);
 }
