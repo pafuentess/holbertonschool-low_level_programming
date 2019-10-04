39401fb35b7b08f5a74b6f9b0fe966918af77b2a
pafuentess <pafuentess@unal.edu.co>
2019-10-04 00:03:40 +0000

    answer6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+* cap_string - capitalize a string
+* @s: string to capitalize
+* Return: (s) the string capitalizado
+*/
+
+
+char *cap_string(char *s)
+{
+	int a;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		if (s[a] == ' ' || s[a] == '\n')
+		{
+			if (s[a + 1] >= 97 && s[a + 1] <= 122)
+			{
+				s[a + 1] = s[a + 1] - 32;
+			}
+		}
+		++a;
+	}
+
+	return (s);
+}
