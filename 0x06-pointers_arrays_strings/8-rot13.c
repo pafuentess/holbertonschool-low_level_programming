dbdf7883c918f56fe914a59656125d619038e921
pafuentess <pafuentess@unal.edu.co>
2019-10-04 02:16:45 +0000

    answer8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+* rot13 - codes a string in rot13
+*@p: function parameter
+* Return: p
+*/
+
+
+char *rot13(char *p)
+{
+	int a, b;
+
+	char ini[] = "AaBbCcDdEeFfGgHhIiJjKkLlMm";
+	char fin[] = "NnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+	for (a = 0 ; p[a] != '\0' ; a++)
+	{
+		for (b = 0 ; ini[b] <= '\0' ; b++)
+		{
+			if (p[a] == ini[b])
+			{
+				p[a] = fin[b];
+			}
+		}
+	}
+
+	return (p);
+}
