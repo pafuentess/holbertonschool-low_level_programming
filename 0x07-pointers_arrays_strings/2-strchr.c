077a719faa058ffb0b507fb9f80d3265731fa99c
pafuentess <pafuentess@unal.edu.co>
2019-10-07 16:23:16 +0000

    answer2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* _strchr - locates a character in a string.
+* @s: where to find
+* @c: character to find
+* Return: s, when find the character
+*/
+
+
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		s++;
+
+		if (s[a] == c)
+		{
+			break;
+		}
+		++a;
+	}
+	return (s);
+}
