2b696214ae5434c3e9231e0e87619994d5dc2e8e
pafuentess <pafuentess@unal.edu.co>
2019-10-08 01:54:37 +0000

    answer9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* set_string - change the value of s
+* @s: the value who will change
+* @to: the value to change s
+*/
+
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
