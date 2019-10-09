ff7ff9c2999fefb008ff8bf3dcf10772a286d7cd
pafuentess <pafuentess@unal.edu.co>
2019-10-09 00:05:02 +0000

    answer5 after one day workin in it

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+* _strstr - unction finds the first occurrence of the substring
+* @haystack: string to read
+* @needle: string to compare
+* Return: return the match after the match
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int cont;
+
+	cont = 0;
+
+	while (*haystack != '\0')
+	{
+		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
+		{
+			haystack++;
+			needle++;
+			cont++;
+		}
+
+		if (*needle == '\0')
+		{
+			return (haystack - cont);
+		}
+
+		haystack = haystack - cont + 1;
+		needle = needle - cont;
+	}
+
+	haystack = 0;
+
+	return (haystack);
+}
