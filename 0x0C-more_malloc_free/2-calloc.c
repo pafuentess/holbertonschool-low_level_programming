9142f1c3081dc96874c2b94f09220f82649db818
pafuentess <pafuentess@unal.edu.co>
2019-10-17 12:44:12 -0500

    betty

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,10 +1,17 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+* _calloc - function that allocates memory for an array
+* @nmemb: elements
+* @size: size elements
+* Return: ar: pointer with memory allocate
+*/
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ar;
-	
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
