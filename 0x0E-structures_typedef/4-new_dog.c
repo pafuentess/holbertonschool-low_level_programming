92357f6c140a9baf4ffe8abbdbc8c6b0c1e57c33
pafuentess <pafuentess@unal.edu.co>
2019-10-21 17:04:26 -0500

    null conditions

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -75,14 +75,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = malloc(len1 + 1);
 	if (new_dog->name == NULL)
 	{
-		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
 	new_dog->owner = malloc(len2 + 1);
 	if (new_dog->owner == NULL)
 	{
-		free(new_dog->owner);
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
