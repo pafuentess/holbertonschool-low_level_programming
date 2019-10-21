292bd51e48bebe60df085d73338317c29556ea60
pafuentess <pafuentess@unal.edu.co>
2019-10-21 13:12:16 -0500

    answer 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef Z
+#define Z
+
+/**
+* struct dog - define a nwe type of dog
+* @name: name of dog
+* @owner: owner of dog
+* @age: dog's age
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
