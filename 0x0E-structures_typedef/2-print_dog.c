aa7e7bf265138ca358e69efca345c194e2e66457
pafuentess <pafuentess@unal.edu.co>
2019-10-21 14:14:04 -0500

    null float condition

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age != d->age)
+	if (!(d->age))
 		printf("Age: (nil)\n");
 	else
 		printf("Age: %.6f\n", d->age);
