846ab4d29ae78b0f11df45efee4e19a0d3d6a3cb
pafuentess <pafuentess@unal.edu.co>
2019-10-23 11:01:44 -0500

    from 0 to 2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+* array_iterator - function that executes a function
+* given as a parameter on each element of an array.
+* @array: array to evaluate
+* @size: lenght of array
+* @action: pointer to function
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+* int_index - function that searches for an integer.
+* @array: where to search the integer
+* @size: lenght of array
+* @cmp: function pointer
+* Return: return the index of match
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, res;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0 ; i < size ; i++)
+	{
+		res = cmp(array[i]);
+
+		if (res)
+			return (i);
+	}
+
+	return (-1);
+}
