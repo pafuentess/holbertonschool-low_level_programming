fbd1adca8cbf4d079b406afeff797e0ce97301aa
pafuentess <pafuentess@unal.edu.co>
2019-10-26 15:22:30 -0500

    betty.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,13 +3,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+* struct op - Struct op
+*
+* @op: The operator
+* @f: The function associated
+*/
+
 typedef struct op
 {
-        char *op;
-        void (*f)(va_list);
+	char *op;
+	void (*f)(va_list);
 } op_t;
-
-
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
