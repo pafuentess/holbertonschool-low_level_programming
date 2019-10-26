4b6335f34855b753d92862d7b6b33bb13b611132
pafuentess <pafuentess@unal.edu.co>
2019-10-26 12:51:51 -0500

    change strategy

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,86 +1,45 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
-*printchar - print a character
-*@lista: parameters to print
-*/
-
-void printchar(va_list lista)
-{
-	printf("%c", va_arg(lista, int));
-}
-
-/**
-*printint - print an integer
-*@lista: parameters to print
-*/
-
-void printint(va_list lista)
-{
-	printf("%d", va_arg(lista, int));
-}
-
-/**
-*printfloat - print a float
-*@lista: parameter to print
-*/
-void printfloat(va_list lista)
-{
-	printf("%f", va_arg(lista, double));
-}
-
-/**
-*printstring - print a string
-*@lista: parameter to print
-*/
-
-void printstring(va_list lista)
-{
-	char *st;
-
-	st = va_arg(lista, char *);
-	if (st == NULL)
-		st = "(nil)";
-	printf("%s", st);
-}
-
-/**
-*print_all - function that prints anything.
-*@format: parameters to print
+* print_all - function that prints anything.
+* @format: list of types of arguments passed to the function
 */
 
 void print_all(const char * const format, ...)
 {
-	op_t ops[] = {
-		{"c", printchar},
-		{"i", printint},
-		{"f", printfloat},
-		{"s", printstring},
-		{NULL, NULL} };
-
 	va_list lista;
-	int i, j = 0;
+	int i;
+	char *st;
 
 	va_start(lista, format);
-	while (format != NULL && format[j] != '\0')
+
+	i = 0;
+	while (format[i] != '\0')
 	{
-		i = 0;
-		while (ops[i].op != NULL)
+		switch (format[i])
 		{
-			if (format[j] == *(ops[i].op))
-			{
-				(ops[i].f)(lista);
-			}
-			i++;
+			case 'c':
+				printf("%c", va_arg(lista, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(lista, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(lista, double));
+				break;
+			case 's':
+				st = va_arg(lista, char *);
+				if (st == NULL)
+					st = "(nil)";
+				printf("%s", st);
+				break;
 		}
-		if ((format[j] == 'c' || format[j] == 'i' || format[j] == 'f' ||
-			format[j] == 's') && format[j + 1] != '\0')
-		{
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+			format[i] == 's') && format[i + 1] != '\0')
 			printf(", ");
-		j++;
+		i++;
 	}
 	printf("\n");
-	va_end(lista);
 }
