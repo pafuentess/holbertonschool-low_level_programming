0875701918aaf113fa2be5ca82399c3e3f3d0b58
pafuentess <pafuentess@unal.edu.co>
2019-10-31 13:20:56 -0500

    len fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,22 +11,17 @@
 
 size_t print_list(const list_t *h)
 {
-	int i;
+	unsigned int i = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-			h = h->next;
-			i++;
-		}
 		else
-		{	
 			printf("[%i] %s\n", h->len, h->str);
-			h = h->next;
-			i++;
-		}
+
+		h = h->next;
+		i++;
 	}
 		return (i);
 }
