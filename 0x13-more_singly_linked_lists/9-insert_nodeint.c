16b342c9dfff560ff61360e2f868eef20223c8c0
pafuentess <pafuentess@unal.edu.co>
2019-11-05 07:21:14 -0500

    add 0 condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	st = *head;
 
+	if (idx == 0)
+	{
+		new_node->next = st;
+		*head = new_node;
+		return (new_node);
+	}
+
 	for (i = 0 ; i < idx - 1 ; i++)
 	{
 		if (st == NULL || st->next == NULL)
