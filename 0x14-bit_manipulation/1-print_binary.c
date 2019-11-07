1074bd3b7b724de28ef1ab0776526203697b7ecd
pafuentess <pafuentess@unal.edu.co>
2019-11-07 14:37:47 -0500

    task 1 fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,21 +10,12 @@
 
 void print_binary(unsigned long int n)
 {
-	int st[100000], j, i = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
+	if (n & 1)
+		_putchar('1');
+	else
 		_putchar('0');
-	while (n != 0)
-	{
-		st[i] = n & 1;
-		n = (n >> 1);
-		i++;
-	}
-
-	for (j = (i - 1) ; j >= 0 ; j--)
-	{
-		_putchar(st[j] + '0');
-	}
-
 }
 
