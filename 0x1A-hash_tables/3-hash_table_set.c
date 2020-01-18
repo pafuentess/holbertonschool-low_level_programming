61dcc3ba8f9ab7239c25ebd9950e9c65cf62b896
pafuentess <pafuentess@unal.edu.co>
2020-01-18 16:07:37 -0500

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,12 +3,72 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+/**
+* add_node - add new node at the inicio
+* @key: list
+* @value: string to copy in the new node
+* Return: new node
+*/
+
+hash_node_t *add_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
+* hash_table_set - adds an element to the hash table.
+* @ht: hash table you want to add or update
+* @key: key is the key
+* @value: the value associated with the key.
+* Return: (1) if succeeded, 0 otherwise
+*/
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned int index = 0;
+	unsigned long int index = 0;
+	hash_node_t *node, *tmp;
 
-	index = hash_djb2(key);
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
 
-	ht->array[index]->vale = stdrup(value)
+	node = NULL;
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(node->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	node = add_node(key, value);
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
+	return (1);
 }
